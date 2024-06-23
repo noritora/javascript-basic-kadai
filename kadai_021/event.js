13e94664d605bab536a628e41a59c54ee9b4a457
nori3 <norito@toralab.org>
2024-06-23 23:09:23 +0900

    retry

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,8 +1,15 @@
 const btn = document.getElementById('btn');
 
-setTimeout(() => {
-    btn.addEventListener('click', () => {
-        const textH2 = document.getElementById('text');
+// setTimeout(() => {
+//     btn.addEventListener('click', () => {
+//         const textH2 = document.getElementById('text');
+//         textH2.textContent = 'ボタンをクリックしました';
+//     });    
+// }, 2100);
+
+btn.addEventListener('click', () => {
+    const textH2 = document.getElementById('text');
+    setTimeout(() => {
         textH2.textContent = 'ボタンをクリックしました';
-    });    
-}, 2100);
+    }, 2000);
+});  
