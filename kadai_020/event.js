35be66c0f7f75f14dbb7bf600a7b0ce9ddd9c97c
nori3 <norito@toralab.org>
2024-06-23 21:45:02 +0900

    kadai020

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    const textH2 = document.getElementById('text');
+    textH2.textContent = 'ボタンをクリックしました';
+});
